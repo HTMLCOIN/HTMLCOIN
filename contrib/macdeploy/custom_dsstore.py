15268bba3b96bf2d89fb500444007ad18a72561e
Peter Bushnell <bushsolo@gmail.com>
2017-10-03 21:05:35 +0100

    Mac deploy

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -45,8 +45,8 @@ alias = Alias.from_bytes(icvp['backgroundImageAlias'])
 alias.volume.name = package_name_ns
 alias.volume.posix_path = '/Volumes/' + package_name_ns
 alias.volume.disk_image_alias.target.filename = package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00qtumuser:\x00Documents:\x00qtum:\x00qtum:\x00' + package_name_ns + '.temp.dmg'
-alias.volume.disk_image_alias.target.posix_path = 'Users/qtumuser/Documents/qtum/qtum/' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.carbon_path = 'Macintosh HD:Users:\x00htmlcoinuser:\x00Documents:\x00htmlcoin:\x00htmlcoin:\x00' + package_name_ns + '.temp.dmg'
+alias.volume.disk_image_alias.target.posix_path = 'Users/htmlcoinuser/Documents/htmlcoin/htmlcoin/' + package_name_ns + '.temp.dmg'
 alias.target.carbon_path = package_name_ns + ':.background:\x00background.tiff'
 icvp['backgroundImageAlias'] = biplist.Data(alias.to_bytes())
 ds['.']['icvp'] = icvp
@@ -54,7 +54,7 @@ ds['.']['icvp'] = icvp
 ds['.']['vSrn'] = ('long', 1)
 
 ds['Applications']['Iloc'] = (370, 156)
-ds['Qtum-Qt.app']['Iloc'] = (128, 156)
+ds['HTMLCOIN-Qt.app']['Iloc'] = (128, 156)
 
 ds.flush()
 ds.close()
