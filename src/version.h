d76f0b8e383ffda70061961892407c0ffc8ba8a8
Peter Bushnell <bushsolo@gmail.com>
2017-11-22 19:26:27 +0000

    v2.0.0.1

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70000;
+static const int PROTOCOL_VERSION = 70001;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
