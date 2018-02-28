80df6e6170e53e0dc249db59943bbf3d4ad0ff6b
Peter Bushnell <bushsolo@gmail.com>
2018-02-28 17:27:29 +0000

    Fix About page crash

--- src/init.cpp
+++ src/init.cpp
@@ -528,7 +528,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/HTMLCOIN/HTMLCOIN>";
     const std::string URL_WEBSITE = "<https://htmlcoin.com>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i"), COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2017, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
