e68a98ce79e95baa2f25a90dee91d48170111dea
Peter Bushnell <bushsolo@gmail.com>
2018-06-26 14:23:20 +0100

    Build, version, graphics, seeds and dist

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.qtum.Qtum-Qt";
+        return @"com.htmlcoin.HTMLCOIN-Qt";
     } else {
         return [self __bundleIdentifier];
     }
