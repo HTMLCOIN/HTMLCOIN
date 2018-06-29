0da112e1c77d044270d9bc635dae11be155d06f6
Peter Bushnell <bushsolo@gmail.com>
2018-06-29 15:33:24 +0100

    No CUPS

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -80,6 +80,7 @@ $(package)_config_opts += -silent
 $(package)_config_opts += -v
 $(package)_config_opts += -no-feature-printer
 $(package)_config_opts += -no-feature-printdialog
+$(package)_config_opts += -no-cups
 
 ifneq ($(build_os),darwin)
 $(package)_config_opts_darwin = -xplatform macx-clang-linux
