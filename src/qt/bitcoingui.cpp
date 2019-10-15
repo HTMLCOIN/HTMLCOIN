b00e7ecf3f923f1cad6f79eb1a5a20c08d5a7a31
Peter Bushnell <bushsolo@gmail.com>
2019-10-15 17:02:09 +0100

    Fix parsing of release page

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -659,7 +659,7 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel)
         if(_clientModel->getOptionsModel()->getCheckForUpdates() && qtumVersionChecker->newVersionAvailable())
         {
             QString link = QString("<a href=%1>%2</a>").arg(QTUM_RELEASES, QTUM_RELEASES);
-            QString message(tr("New version of HTMLCOIN wallet is available on the HTMLCOIN source code repository: <br /> %1. <br />It is recommended to download it and update this application").arg(link));
+            QString message(tr("New version of HTMLCOIN wallet is available on the HTMLCOIN source code repository: <br /> %1 <br />It is recommended to download it and update this application").arg(link));
             QMessageBox::information(this, tr("Check for updates"), message);
         }
 
