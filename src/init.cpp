46a4255af3646574d8f1c2e41b4bd66c265b2d04
Peter Bushnell <bushsolo@gmail.com>
2019-10-15 12:35:08 +0100

    Build, version, dist, seeds and icons

--- src/init.cpp
+++ src/init.cpp
@@ -576,10 +576,10 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/qtumproject/qtum>";
-    const std::string URL_WEBSITE = "<https://qtum.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/HTMLCOIN/HTMLCOIN>";
+    const std::string URL_WEBSITE = "<https://htmlcoin.com>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i"), COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2014, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
